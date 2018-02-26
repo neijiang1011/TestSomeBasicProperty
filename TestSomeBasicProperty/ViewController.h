805e88917d6fd4cead1ef7f9d56c9b7467bf580f
yetao ye <yetaoye@yetaodemacbook-pro.local>
2018-02-26 14:29:27 +0800

    Initial Commit

---
+++ TestSomeBasicProperty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSomeBasicProperty
+//
+//  Created by yetao ye on 2018/2/26.
+//  Copyright © 2018年 com.changhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestSomeBasicPropertyTests/TestSomeBasicPropertyTests.m
@@ -0,0 +1,39 @@
+//
+//  TestSomeBasicPropertyTests.m
+//  TestSomeBasicPropertyTests
+//
+//  Created by yetao ye on 2018/2/26.
+//  Copyright © 2018年 com.changhong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestSomeBasicPropertyTests : XCTestCase
+
+@end
+
+@implementation TestSomeBasicPropertyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
