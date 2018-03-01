890ef1ad51bd723b7710b84c283a156b277bebc5
yetao ye <yetaoye@yetaodemacbook-pro.local>
2018-03-01 16:06:08 +0800

    discard one function.

--- TestSomeBasicProperty/MapForItineraryViewController.m
+++ TestSomeBasicProperty/MapForItineraryViewController.m
@@ -17,8 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSLog(@"Map for Itinerary.");
-
 }
 
 - (void)didReceiveMemoryWarning {
