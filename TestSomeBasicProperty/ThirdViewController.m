c75cf879fdfd7ab8b2ad10ff9a57a3b55f6c0aaf
yetao ye <yetaoye@yetaodemacbook-pro.local>
2018-03-01 16:14:10 +0800

    new a label.

--- TestSomeBasicProperty/ThirdViewController.m
+++ TestSomeBasicProperty/ThirdViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor blueColor];
     self.title = @"Third";
+    
+    UILabel *label = [[UILabel alloc]init];
 }
 
 - (void)didReceiveMemoryWarning {
