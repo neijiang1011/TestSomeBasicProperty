fcd892dbd4619480e578aec0068a2b0429bfa0c5
yetao ye <yetaoye@yetaodemacbook-pro.local>
2018-03-01 15:17:56 +0800

    Add map feature.

---
+++ TestSomeBasicProperty/MapForItineraryViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapForItineraryViewController.h
+//  TestSomeBasicProperty
+//
+//  Created by yetao ye on 2018/3/1.
+//  Copyright © 2018年 com.changhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapForItineraryViewController : UIViewController
+
+@end
